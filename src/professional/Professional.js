c838b91fc45eb604d81c569139a18e8b1262513e
lim-yogesh2001 <yogeshlim2001@gmail.com>
2023-06-14 21:53:02 +0545

    okay

--- src/professional/Professional.js
+++ src/professional/Professional.js
@@ -1,42 +1,8 @@
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import Home from './Home/Home';
-// import Charts from "./Charts/Charts";
-// import ProfSecurity from "./ProfSecurity/ProfSecurity";
-// import ProfPayment from "./ProfPayment/ProfPayment";
-// import ProfPaymentHistory from "./ProfPaymentHistory/ProfPaymentHistory";
-// import ProfDeleteAcc from "./ProfDeleteAcc/PorfDeleteAcc";
+
 
 export default function Professional() {
-    // const professionalRouter = createBrowserRouter([
-    //     {
-    //         path: '/professional/',
-    //         element: <Home />,
-    //         children: [
-    //             {
-    //                 path: '/professional/',
-    //                 element: <Charts />
-    //             },
-    //             {
-    //                 path: '/professional/security/',
-    //                 element: <ProfSecurity />
-    //             },
-    //             {
-    //                 path: '/professional/paymentMethod/',
-    //                 element: <ProfPayment />
-    //             },
-    //             {
-    //                 path: '/professional/paymentHistory/',
-    //                 element: <ProfPaymentHistory />
-    //             },
-    //             {
-    //                 path: '/professional/deleteAcc/',
-    //                 element: <ProfDeleteAcc />
-    //             }
-    //         ]
-    //     }
-    // ])
 
     return (
         <Home />
